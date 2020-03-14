b96f215c89ffe77dd30c85731a5dee3c78d7d044
valera17496 <valdo17496@gmail.com>
2020-03-14 23:21:12 +0500

    Добавлена реализация класса-калькулятора выражений

---
+++ Calculator.Domain/Calculators/Calculator.cs
@@ -0,0 +1,39 @@
+﻿using Calculator.Domain.Parsers;
+using Calculator.Domain.Validators;
+using System;
+
+namespace Calculator.Domain.Calculators
+{
+	public class Calculator<T> : ICalculator<T>
+	{
+		private readonly IExpressionsValidator<T> _validator;
+
+		private readonly IParser<T> _parser;
+
+		public Calculator(IParser<T> parser, IExpressionsValidator<T> validator)
+		{
+			_validator = validator;
+			_parser = parser;
+		}
+
+		public T Calculate(string expression)
+		{
+			try
+			{
+				if (_validator.BracketsValidation(expression))
+					throw new ExpressionFormatException("В данном выражении не одинаковое количество открытых и закрытых скобок.");
+
+				if (_validator.ValidateAvailableOperation(expression, _parser.AvailableOperations))
+					throw new ExpressionFormatException("В данном выражении обнаружены операции, которые в данный момент не определены.");
+
+				var reverseExpression = _parser.Parse(expression);
+
+				return _parser.Calculate(reverseExpression);
+			}
+			catch (Exception ex)
+			{
+				throw ex;
+			}			
+		}
+	}
+}
---
+++ Calculator.Domain/Calculators/ExpressionFormatException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Calculator.Domain.Calculators
+{
+	public class ExpressionFormatException : Exception
+	{
+		public ExpressionFormatException(string message) : base(message)
+		{
+
+		}
+	}
+}
